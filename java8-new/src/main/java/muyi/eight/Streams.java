bafd2353880d06fa79dccbfaa50cac3901d71c0c
muyi-macpro <17621660286@163.com>
2018-06-04 00:40:24 +0800

    streams ;

--- java8-new/src/main/java/muyi/eight/Streams.java
+++ java8-new/src/main/java/muyi/eight/Streams.java
@@ -1,9 +1,10 @@
 package muyi.eight;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BinaryOperator;
 import java.util.stream.Stream;
 
 /**
@@ -41,16 +42,21 @@ public class Streams {
         stringCollection.stream()
                 .filter(s -> s.startsWith("a"))
                 .forEach(System.out::println);
+        System.out.println("------------");
 
         stringCollection.stream()
                 .sorted()
                 .filter(s -> s.startsWith("a"))
                 .forEach(System.out::println);
+        System.out.println("------------");
+
 
         stringCollection.stream()
                 .map(String::toUpperCase)
                 .sorted()
                 .forEach(System.out::println);
+        System.out.println("------------");
+
 
         /* match */
         boolean anyStartsWithA = stringCollection.stream().anyMatch(s -> s.startsWith("a"));
@@ -61,8 +67,50 @@ public class Streams {
 
         boolean noneStartsWithZ = stringCollection.stream().noneMatch(s -> s.startsWith("z"));
         assert noneStartsWithZ;
-        
+
+        /* count */
+        long countStartWithB = stringCollection
+                .stream()
+                .filter(s -> s.startsWith("b"))
+                .count();
+        assert countStartWithB == 3L;
+
+        Optional<String> stringOptional = stringCollection.stream()
+                .reduce((s1, s2) -> s1 + "#" + s2);
+        System.out.println(stringOptional.orElse("null"));
+        System.out.println("------------");
 
     }
+
+    /**
+     * Parallel Streams
+     */
+    void testParallelStream() {
+        int max = 1000000;
+        List<String> list = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            UUID uuid = UUID.randomUUID();
+            list.add(uuid.toString());
+        }
+
+        long t0 = System.nanoTime();
+        long count = list.stream().sorted().count();
+        assert count == max;
+        long t1 = System.nanoTime();
+        long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+        System.out.println(String.format("sequential sort took: %d ms", millis));
+
+
+        long t3 = System.nanoTime();
+        long count1 = list.parallelStream().sorted().count();
+        assert count1 == max;
+        long t4 = System.nanoTime();
+
+        millis = TimeUnit.NANOSECONDS.toMillis(t4 - t3);
+        System.out.println(String.format("parallel sort took: %d ms", millis));
+    }
+
+    /* maps */
+    
 
 }
---
+++ java8-new/src/test/java/muyi/eight/StreamsTest.java
@@ -0,0 +1,26 @@
+package muyi.eight;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: muyi-macpro
+ * @datetime: 2018/6/3 下午11:56
+ * @description:
+ */
+public class StreamsTest {
+
+    @Test
+    public void main() {
+        new Streams().main();
+    }
+
+
+    @Test
+    public void testParallelStream() {
+        new Streams().testParallelStream();
+    }
+}
