e6f6aab73b7c25690b99f9ee7a6d8152fe87aa19
muyi-macpro <17621660286@163.com>
2018-08-15 23:33:50 +0800

    add japi-doc 但不是一个优雅的文档方式。

---
+++ japi-doc/src/main/java/muyi/jdoc/Application.java
@@ -0,0 +1,17 @@
+package muyi.jdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: Jimu Yang.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ japi-doc/src/main/java/muyi/jdoc/controller/UserController.java
@@ -0,0 +1,14 @@
+package muyi.jdoc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: Jimu Yang.
+ */
+@RestController
+public class UserController {
+
+
+}
