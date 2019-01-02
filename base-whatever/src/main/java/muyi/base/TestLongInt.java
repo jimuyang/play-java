40befb8d8933a316cad619782ea86463021dbbd5
muyi-macpro <17621660286@163.com>
2019-01-02 23:54:16 +0800

    test java long.intValue() 是彻底的丢失

---
+++ base-whatever/src/main/java/muyi/base/TestLongInt.java
@@ -0,0 +1,15 @@
+package muyi.base;
+
+/**
+ * @author: Jimu Yang
+ * @date: 2018/12/30 6:12 PM
+ * @descricption: want more.
+ */
+public class TestLongInt {
+
+    public static void main(String[] args) {
+        Long l = 4956765210L;
+        int i = l.intValue();
+        System.out.println(i);
+    }
+}
