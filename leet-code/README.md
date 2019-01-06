d32c114aaabfd420c7b393b499c9ecb6daaef01c
muyi-macpro <17621660286@163.com>
2019-01-06 20:47:50 +0800

    leetcode 21: merge two sorted linked lists

---
+++ leet-code/README.md
@@ -0,0 +1,1 @@
+顾名思义 刷leetcode java实现和被虐
---
+++ leet-code/src/main/java/muyi/leetcode/L21Merge2SortedLists.java
@@ -0,0 +1,83 @@
+package muyi.leetcode;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author: Jimu Yang
+ * @date: 2019/1/6 8:26 PM
+ * @descricption: want more.
+ */
+public class L21Merge2SortedLists {
+
+    /**
+     * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+     * <p>
+     * Example:
+     * Input: 1->2->4, 1->3->4
+     * Output: 1->1->2->3->4->4
+     */
+
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode node;
+        if (l1.val < l2.val) {
+            node = l1;
+            l1 = l1.next;
+        } else {
+            node = l2;
+            l2 = l2.next;
+        }
+        ListNode start = node;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        if (l1 == null) {
+            node.next = l2;
+        }
+        if (l2 == null) {
+            node.next = l1;
+        }
+        return start;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    @Test
+    public void test() {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = this.mergeTwoLists(l1, l2);
+        return;
+    }
+
+}
