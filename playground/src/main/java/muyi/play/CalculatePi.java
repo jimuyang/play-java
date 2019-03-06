d6326356e28e1d73f2630edc879fd146ef4a6933
muyi_home <17621660286@163.com>
2019-03-06 23:37:39 +0800

    蒙特卡洛算法 计算PI

---
+++ playground/src/main/java/muyi/play/CalculatePi.java
@@ -0,0 +1,36 @@
+package muyi.play;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * @author: Jimu Yang
+ * @date: 2019/3/6 11:24 PM
+ * @descricption: want more.
+ * <p>
+ * 蒙特卡洛算法计算 π
+ */
+public class CalculatePi {
+
+    public float getPi() {
+        Random random = new Random();
+        int count = 0;
+        for (int i = 0; i < 100000000; i++) {
+
+            double x = random.nextDouble() * 2 - 1;
+            double y = random.nextDouble() * 2 - 1;
+
+            if (x * x + y * y <= 1) {
+                count++;
+            }
+        }
+        return (float) count * 4 / 100000000;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(getPi());
+    }
+
+}
