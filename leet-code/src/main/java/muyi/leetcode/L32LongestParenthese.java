8abfe4ccee6a4cb1e72523a00c8311d5cf3e0509
muyi_home <17621660286@163.com>
2019-03-10 00:58:58 +0800

    leetcode 32：最长合理括号子串

--- leet-code/src/main/java/muyi/leetcode/L32LongestParenthese.java
+++ leet-code/src/main/java/muyi/leetcode/L32LongestParenthese.java
@@ -2,6 +2,8 @@ package muyi.leetcode;
 
 import org.junit.Test;
 
+import java.util.Stack;
+
 /**
  * @author: Jimu Yang
  * @date: 2019/3/8 12:08 AM
@@ -11,30 +13,68 @@ import org.junit.Test;
  */
 public class L32LongestParenthese {
 
-
-    public int longestValidParentheses1(String s) {
+    public int longestValidParentheses2(String s) {
         if (s == null || s.length() == 0) return 0;
-        char[] chararr = s.toCharArray();
-        int[] intarr = new int[chararr.length];
+        char[] chars = s.toCharArray();
 
-        int stack = 0;
-        for (int i = 0; i < chararr.length; i++) {
-            if (chararr[i] == '(') {
-                stack++;
+        int longest = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(') {
+                stack.push(0);
             } else {
-                if (stack > 0) {
-                    // 凑成了对子
-                    stack--;
-                    intarr[i] = stack + 1; // 记录此时栈深 +1因int[]初始为0
+                if (stack.size() > 1) {
+                    int pair = stack.pop();
+                    int old = stack.pop();
+                    stack.push(old + pair + 1);
                 } else {
-                    // 破坏
-                    intarr[i] = -1; // 竖墙 -1
+                    // 此处认为一段检查已完结
+                    for (Integer pair : stack) {
+                        longest = longest > pair ? longest : pair;
+                    }
+                    stack.clear();
+                    stack.push(0);
                 }
             }
         }
 
-        // 至此 转化为求最远点问题
+        // 此处认为一段检查已完结
+        for (Integer pair : stack) {
+            longest = longest > pair ? longest : pair;
+        }
+
+        return longest * 2;
     }
+
+
+//    public int longestValidParentheses1(String s) {
+//        if (s == null || s.length() == 0) return 0;
+//        char[] chararr = s.toCharArray();
+//        int[] intarr = new int[chararr.length];
+//
+//        int stack = 0;
+//        for (int i = 0; i < chararr.length; i++) {
+//            if (chararr[i] == '(') {
+//                stack++;
+//            } else {
+//                if (stack > 0) {
+//                    // 凑成了对子
+//                    stack--;
+//                    intarr[i] = stack + 1; // 记录此时栈深 +1因int[]初始为0
+//                } else {
+//                    // 破坏
+//                    intarr[i] = -1; // 竖墙 -1
+//                }
+//            }
+//        }
+//
+//        // 至此 转化为求最远点问题
+//
+//    }
 
     /**
      * 失败解法 ()(()
@@ -66,8 +106,8 @@ public class L32LongestParenthese {
 
     @Test
     public void test() {
-        System.out.println(this.longestValidParentheses("()(()"));
-        System.out.println(this.longestValidParentheses("()()("));
+        System.out.println(this.longestValidParentheses2("()(()"));
+        System.out.println(this.longestValidParentheses2("()()("));
     }
 
 }
