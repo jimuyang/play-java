b1b4fd8d3b5c42f3a67a9644d114e108ba74da5e
杨帆 <fan.yang19@ele.me>
2019-09-07 15:17:01 +0800

    feat: 一个最简单的线程池实现

--- base-whatever/src/main/java/muyi/base/thread/SimpleThreadPool.java
+++ base-whatever/src/main/java/muyi/base/thread/SimpleThreadPool.java
@@ -1,7 +1,10 @@
 package muyi.base.thread;
 
-import java.util.LinkedList;
-import java.util.List;
+import javafx.scene.control.Pagination;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author: Yang Fan
@@ -10,49 +13,107 @@ import java.util.List;
  */
 public class SimpleThreadPool<Job extends Runnable> implements ThreadPool<Job> {
 
-    // 线程池最大限制数
+    /**
+     * 线程池最大限制数
+     */
     private static final int MAX_WORKER_COUNT = 10;
 
-    // 线程池默认数量
+    /**
+     * 线程池默认数量
+     */
     private static final int DEFAULT_WORKER_COUNT = 5;
 
-    // 线程池最小数量
+    /**
+     * 线程池最小数量
+     */
     private static final int MIN_WORKER_COUNT = 1;
 
-    // 工作列表
+    /**
+     * 工作列表
+     */
     private final LinkedList<Job> jobs = new LinkedList<>();
 
-    // 工作线程
-//    private final List<Worker> workers =
+    /**
+     * 工作线程
+     */
+    private final List<Worker> workers = Collections.synchronizedList(new ArrayList<>());
 
+    /**
+     * 工作线程的数量
+     */
+    private int workerNum = DEFAULT_WORKER_COUNT;
+
+    /**
+     * 线程编号生成
+     */
+    private AtomicLong threadNum = new AtomicLong();
+
+    public SimpleThreadPool() {
+        initializeWorkers(DEFAULT_WORKER_COUNT);
+    }
+
+    public SimpleThreadPool(int workerNum) {
+        workerNum = workerNum > MAX_WORKER_COUNT ? MAX_WORKER_COUNT :
+                workerNum < MIN_WORKER_COUNT ? MIN_WORKER_COUNT : workerNum;
+        initializeWorkers(workerNum);
+    }
+
+
+    /**
+     * 初始化线程工作者
+     */
+    private void initializeWorkers(int num) {
+        for (int i = 0; i < num; i++) {
+            Worker worker = new Worker();
+            workers.add(worker);
+            Thread thread = new Thread(worker, "ThreadPool-worker-" + threadNum.incrementAndGet());
+            thread.start();
+        }
+    }
 
     @Override
-
     public void execute(Job job) {
-
+        if (job != null) {
+            synchronized (jobs) {
+                jobs.addLast(job);
+                jobs.notify();
+            }
+        }
     }
 
     @Override
     public void shutdown() {
-
+        for (Worker worker : workers) {
+            worker.shutdown();
+        }
     }
 
     @Override
     public void addWorkers(int num) {
-
+        // ??? 为啥这里要加锁jobs
+        synchronized (jobs) {
+            if (num + workerNum > MAX_WORKER_COUNT) {
+                num = MAX_WORKER_COUNT - workerNum;
+            }
+            initializeWorkers(num);
+            workerNum += num;
+        }
     }
 
     @Override
     public void removeWorkers(int num) {
-
+        // todo
     }
 
     @Override
     public int countWaitJobs() {
-        return 0;
+        return jobs.size();
     }
 
 
+    /**
+     * 工作者 负责消费任务
+     */
     class Worker implements Runnable {
         // 是否工作
         private volatile boolean running = true;
